c6267734006f39efbcd4b90b39eed9518cbe868d
Andrew Wilk <andrewwilk@client0006-hnt.vpn.private.upenn.edu>
2020-07-14 16:13:19 -0500

    clearing dir, pulling files, writing tar

--- r2rpull.py
+++ r2rpull.py
@@ -4,6 +4,8 @@ from dotenv import load_dotenv
 import os
 import os.path
 import tarfile
+import shutil
+import datetime
 
 load_dotenv()
 SERVER = os.getenv('SERVER')
@@ -12,9 +14,12 @@ SOURCE_USER = os.getenv('SOURCE-USER')
 REMOTE_USER = os.getenv('REMOTE-USER')
 PASSWORD = os.getenv('PASSWORD')
 SOURCE_DIR = os.getenv('SOURCE-DIR')
-LOCAL_DIR = os.getenv('LOCAL-DIR')
 REMOTE_DIR = os.getenv('REMOTE-DIR')
 KEY_FILENAME = os.getenv('KEY_FILENAME')
+
+dir_path = os.path.dirname(os.path.realpath('./'))
+LOCAL_DIR = f"{dir_path}/r2r-backup/pulled-files"
+
 
 def createSSHClient(server, port, user, password):
     client = paramiko.SSHClient()
@@ -22,19 +27,48 @@ def createSSHClient(server, port, user, password):
     client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
     # client.connect(server, port, user, password, key_filename)
     client.connect(hostname=server,username=user, password=password)
+    
     return client
 
+def clear_dirs(dir_path):
+    folders = os.listdir(f"{dir_path}/r2r-backup/pulled-files")
+    for folder in folders:
+        if folder == 'bkps':
+            shutil.rmtree(f"{dir_path}/r2r-backup/pulled-files/{folder}")
+
+    files = os.listdir(f"{dir_path}/r2r-backup/output-files")
+    for file in files:
+        if file not in ['README.md', '.gitignore']:
+            os.remove(f"{dir_path}/r2r-backup/output-files/{file}")
+
+
 def make_tarfile(output_filename, source_dir):
+    print('building tar file...')
+
     with tarfile.open(output_filename, "w:gz") as tar:
         tar.add(source_dir, arcname=os.path.basename(source_dir))
 
-ssh = createSSHClient(SERVER, PORT, SOURCE_USER, PASSWORD)
-scp = SCPClient(ssh.get_transport())
+    print('tar file created...')
 
-scp.get(SOURCE_DIR, LOCAL_DIR, recursive=True)
+def main():
+    global dir_path
 
-make_tarfile("/Users/andrewwilk/Desktop/database-projects/r2r-backup/tada", LOCAL_DIR)
+    clear_dirs(dir_path)
 
+    ssh = createSSHClient(SERVER, PORT, SOURCE_USER, PASSWORD)
+    scp = SCPClient( ssh.get_transport() )
+
+    timestamp = str( datetime.date.today() )
+
+    print('Pulling files from db server...')
+    scp.get( SOURCE_DIR, LOCAL_DIR, recursive=True )
+    print('Files pulled from database server...')
+
+    make_tarfile( f"{dir_path}/r2r-backup/output-files/{timestamp}-db-backup.tar.gz", LOCAL_DIR )
+
+
+if __name__ == '__main__':
+    main()
 
 
 
