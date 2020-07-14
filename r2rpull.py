6ed3a22d45a770523f518e23621181bb6d5408b1
Andrew Wilk <andrewwilk@client0006-hnt.vpn.private.upenn.edu>
2020-07-14 11:51:03 -0500

    successful scp from SOURCE

--- r2rpull.py
+++ r2rpull.py
@@ -6,7 +6,8 @@ import os
 load_dotenv()
 SERVER = os.getenv('SERVER')
 PORT = os.getenv('PORT')
-USER = os.getenv('USER')
+SOURCE_USER = os.getenv('SOURCE-USER')
+REMOTE_USER = os.getenv('REMOTE-USER')
 PASSWORD = os.getenv('PASSWORD')
 SOURCE_DIR = os.getenv('SOURCE-DIR')
 LOCAL_DIR = os.getenv('LOCAL-DIR')
@@ -15,13 +16,15 @@ KEY_FILENAME = os.getenv('KEY_FILENAME')
 
 
 
-def createSSHClient(server, port, user, password, key_filename):
+def createSSHClient(server, port, user, password):
     client = paramiko.SSHClient()
     client.load_system_host_keys()
     client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-    # client.connect(server, port, user, password, allow_agent=False,l ook_for_keys=False)
-    client.connect(server, port, user, password, key_filename)
+    # client.connect(server, port, user, password, key_filename)
+    client.connect(hostname=server,username=user, password=password)
     return client
 
-ssh = createSSHClient(SERVER, PORT, USER, PASSWORD, KEY_FILENAME)
-# scp = SCPClient(ssh.get_transport())
+ssh = createSSHClient(SERVER, PORT, SOURCE_USER, PASSWORD)
+scp = SCPClient(ssh.get_transport())
+
+scp.get('/home/andy/text.txt', LOCAL_DIR)
